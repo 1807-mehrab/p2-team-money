e17e3820c92347673f1c0f78970db36bdbf2d17b
Jimmy Duffy <jeduffy3@gmail.com>
2018-08-16 17:37:31 -0500

    Update Daos using JDBC

--- StockInvestment/src/main/java/com/revature/stockinvestment/dao/CompanyStockDaoOracleSqlImpl.java
+++ StockInvestment/src/main/java/com/revature/stockinvestment/dao/CompanyStockDaoOracleSqlImpl.java
@@ -5,7 +5,15 @@
  */
 package com.revature.stockinvestment.dao;
 
+import com.revature.stockinvestment.model.Account;
 import com.revature.stockinvestment.model.CompanyStock;
+import com.revature.stockinvestment.util.ConnectionUtil;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,29 +22,187 @@ import java.util.List;
  */
 public class CompanyStockDaoOracleSqlImpl implements CompanyStockDao { 
 
+    private static final String SQL_INSERT_COMPANY_STOCK 
+            = "INSERT INTO COMPANY_STOCK (COMPANY_NAME, STOCK_PRICE, ACCOUNT_ID) "
+            + "VALUES (?, ?, ?)";
+    
+    private static final String SQL_DELETE_COMPANY_STOCK 
+            = "DELETE FROM COMPANY_STOCK "
+            + "WHERE COMPANY_STOCK_ID = ?"; 
+    
+    private static final String SQL_UPDATE_COMPANY_STOCK 
+            = "UPDATE COMPANY_STOCK "
+            + "SET COMPANY_NAME = ?, STOCK_PRICE = ?, ACCOUNT_ID = ? "
+            + "WHERE COMPANY_STOCK_ID = ?";
+    
+    private static final String SQL_SELECT_COMPANY_STOCK_BY_COMPANY_STOCK_ID 
+            = "SELECT COMPANY_STOCK_ID, COMPANY_NAME, STOCK_PRICE "
+            + "FROM COMPANY_STOCK "
+            + "WHERE COMPANY_STOCK_ID = ?";
+    
+    private static final String SQL_SELECT_ALL_COMPANY_STOCKS 
+            = "SELECT COMPANY_STOCK_ID, COMPANY_NAME, STOCK_PRICE "
+            + "FROM COMPANY_STOCK";
+    
     @Override
     public void addCompanyStock(CompanyStock companyStock) throws SIPersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try (Connection conn = ConnectionUtil.getConnection()) {
+            ps = conn.prepareStatement(SQL_INSERT_COMPANY_STOCK);
+            for (Account account : companyStock.getAccountList()) {
+                ps.setString(1, companyStock.getCompanyName());
+                ps.setDouble(2, companyStock.getStockPrice());
+                ps.setInt(3, account.getAccountId());
+                rs = ps.executeQuery();
+            }
+        } catch (SQLException e) {
+            throw new SIPersistenceException("Could not connect to db.", e);
+        } catch (IOException e) {
+            throw new SIPersistenceException("Could not write to db.", e);
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                throw new SIPersistenceException("Could not close db.", e);
+            }
+        }
     }
 
     @Override
     public void deleteCompanyStock(int companyStockId) throws SIPersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try (Connection conn = ConnectionUtil.getConnection()) {
+            ps = conn.prepareStatement(SQL_DELETE_COMPANY_STOCK);
+            ps.setInt(1, companyStockId);
+            rs = ps.executeQuery();
+        } catch (SQLException e) {
+            throw new SIPersistenceException("Could not connect to db.", e);
+        } catch (IOException e) {
+            throw new SIPersistenceException("Could not write to db.", e);
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                throw new SIPersistenceException("Could not close db.", e);
+            }
+        }
     }
 
     @Override
     public void updateCompanyStock(CompanyStock companyStock) throws SIPersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try (Connection conn = ConnectionUtil.getConnection()) {
+            ps = conn.prepareStatement(SQL_UPDATE_COMPANY_STOCK);
+            for (Account account : companyStock.getAccountList()) {
+                ps.setString(1, companyStock.getCompanyName());
+                ps.setDouble(2, companyStock.getStockPrice());
+                ps.setInt(3, account.getAccountId());
+                ps.setInt(4, companyStock.getCompanyStockId());
+                rs = ps.executeQuery();
+            }
+        } catch (SQLException e) {
+            throw new SIPersistenceException("Could not connect to db.", e);
+        } catch (IOException e) {
+            throw new SIPersistenceException("Could not write to db.", e);
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                throw new SIPersistenceException("Could not close db.", e);
+            }
+        }
     }
 
     @Override
     public CompanyStock getCompanyStockByCompanyStockId(int companyStockId) throws SIPersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        CompanyStock companyStock = null;
+
+        try (Connection conn = ConnectionUtil.getConnection()) {
+            ps = conn.prepareStatement(SQL_SELECT_COMPANY_STOCK_BY_COMPANY_STOCK_ID);
+            ps.setInt(1, companyStockId);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                companyStock = new CompanyStock();
+                companyStock.setCompanyStockId(rs.getInt("COMPANY_STOCK_ID"));
+                companyStock.setCompanyName(rs.getString("COMPANY_NAME"));
+                companyStock.setStockPrice(rs.getDouble("STOCK_PRICE"));
+            }
+        } catch (SQLException e) {
+            throw new SIPersistenceException("Could not connect to db.", e);
+        } catch (IOException e) {
+            throw new SIPersistenceException("Could not read from db.", e);
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                throw new SIPersistenceException("Could not close db.", e);
+            }
+        }
+        return companyStock;
     }
 
     @Override
     public List<CompanyStock> getAllCompanyStocks() throws SIPersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        CompanyStock companyStock = null;
+        List<CompanyStock> companyStocks = new ArrayList<>();
+
+        try (Connection conn = ConnectionUtil.getConnection()) {
+            ps = conn.prepareStatement(SQL_SELECT_ALL_COMPANY_STOCKS);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                companyStock = new CompanyStock();
+                companyStock.setCompanyStockId(rs.getInt("COMPANY_STOCK_ID"));
+                companyStock.setCompanyName(rs.getString("COMPANY_NAME"));
+                companyStock.setStockPrice(rs.getDouble("STOCK_PRICE"));
+                companyStocks.add(companyStock);
+            }
+        } catch (SQLException e) {
+            throw new SIPersistenceException("Could not connect to db.", e);
+        } catch (IOException e) {
+            throw new SIPersistenceException("Could not read from db.", e);
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                throw new SIPersistenceException("Could not close db.", e);
+            }
+        }
+        return companyStocks;
     }
     
 }
--- StockInvestment/src/main/java/com/revature/stockinvestment/dao/MemberDaoOracleSqlImpl.java
+++ StockInvestment/src/main/java/com/revature/stockinvestment/dao/MemberDaoOracleSqlImpl.java
@@ -6,6 +6,13 @@
 package com.revature.stockinvestment.dao;
 
 import com.revature.stockinvestment.model.Member;
+import com.revature.stockinvestment.util.ConnectionUtil;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,29 +21,189 @@ import java.util.List;
  */
 public class MemberDaoOracleSqlImpl implements MemberDao { 
 
+    private static final String SQL_INSERT_MEMBER 
+            = "INSERT INTO MEMBER (FIRST_NAME, LAST_NAME, EMAIL, PASSWORD) "
+            + "VALUES (?, ?, ?, ?)";
+    
+    private static final String SQL_DELETE_MEMBER  
+            = "DELETE FROM MEMBER "
+            + "WHERE MEMBER_ID = ?"; 
+    
+    private static final String SQL_UPDATE_MEMBER  
+            = "UPDATE MEMBER "
+            + "SET FIRST_NAME = ?, LAST_NAME = ?, EMAIL = ? , PASSWORD = ? "
+            + "WHERE MEMBER_ID = ?";
+    
+    private static final String SQL_SELECT_MEMBER_BY_MEMBER_ID 
+            = "SELECT MEMBER_ID, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD "
+            + "FROM MEMBER "
+            + "WHERE MEMBER_ID = ?";
+    
+    private static final String SQL_SELECT_ALL_MEMBERS 
+            = "SELECT MEMBER_ID, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD "
+            + "FROM MEMBER";
+    
     @Override
     public void addMember(Member member) throws SIPersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try (Connection conn = ConnectionUtil.getConnection()) {
+            ps = conn.prepareStatement(SQL_INSERT_MEMBER);
+            ps.setString(1, member.getFirstName());
+            ps.setString(2, member.getLastName());
+            ps.setString(3, member.getEmail());
+            ps.setString(4, member.getPassword());
+            rs = ps.executeQuery();
+        } catch (SQLException e) {
+            throw new SIPersistenceException("Could not connect to db.", e);
+        } catch (IOException e) {
+            throw new SIPersistenceException("Could not write to db.", e);
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                throw new SIPersistenceException("Could not close db.", e);
+            }
+        }
     }
 
     @Override
     public void deleteMember(int memberId) throws SIPersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try (Connection conn = ConnectionUtil.getConnection()) {
+            ps = conn.prepareStatement(SQL_DELETE_MEMBER);
+            ps.setInt(1, memberId);
+            rs = ps.executeQuery();
+        } catch (SQLException e) {
+            throw new SIPersistenceException("Could not connect to db.", e);
+        } catch (IOException e) {
+            throw new SIPersistenceException("Could not write to db.", e);
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                throw new SIPersistenceException("Could not close db.", e);
+            }
+        }
     }
 
     @Override
     public void updateMember(Member member) throws SIPersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try (Connection conn = ConnectionUtil.getConnection()) {
+            ps = conn.prepareStatement(SQL_UPDATE_MEMBER);
+            ps.setString(1, member.getFirstName());
+            ps.setString(2, member.getLastName());
+            ps.setString(3, member.getEmail());
+            ps.setString(4, member.getPassword());
+            ps.setInt(5, member.getMemberId());
+            rs = ps.executeQuery();
+        } catch (SQLException e) {
+            throw new SIPersistenceException("Could not connect to db.", e);
+        } catch (IOException e) {
+            throw new SIPersistenceException("Could not write to db.", e);
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                throw new SIPersistenceException("Could not close db.", e);
+            }
+        }
     }
 
     @Override
     public Member getMemberByMemberId(int memberId) throws SIPersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Member member = null;
+
+        try (Connection conn = ConnectionUtil.getConnection()) {
+            ps = conn.prepareStatement(SQL_SELECT_MEMBER_BY_MEMBER_ID);
+            ps.setInt(1, memberId);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                member = new Member();
+                member.setMemberId(rs.getInt("MEMBER_ID"));
+                member.setFirstName(rs.getString("FIRST_NAME"));
+                member.setLastName(rs.getString("LAST_NAME"));
+                member.setEmail(rs.getString("EMAIL"));
+                member.setPassword(rs.getString("PASSWORD"));
+            }
+        } catch (SQLException e) {
+            throw new SIPersistenceException("Could not connect to db.", e);
+        } catch (IOException e) {
+            throw new SIPersistenceException("Could not read from db.", e);
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                throw new SIPersistenceException("Could not close db.", e);
+            }
+        }
+        return member;
     }
 
     @Override
     public List<Member> getAllMembers() throws SIPersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Member member = null;
+        List<Member> members = new ArrayList<>();
+
+        try (Connection conn = ConnectionUtil.getConnection()) {
+            ps = conn.prepareStatement(SQL_SELECT_ALL_MEMBERS);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                member = new Member();
+                member.setMemberId(rs.getInt("MEMBER_ID"));
+                member.setFirstName(rs.getString("FIRST_NAME"));
+                member.setLastName(rs.getString("LAST_NAME"));
+                member.setEmail(rs.getString("EMAIL"));
+                member.setPassword(rs.getString("PASSWORD"));
+                members.add(member);
+            }
+        } catch (SQLException e) {
+            throw new SIPersistenceException("Could not connect to db.", e);
+        } catch (IOException e) {
+            throw new SIPersistenceException("Could not read from db.", e);
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                throw new SIPersistenceException("Could not close db.", e);
+            }
+        }
+        return members;
     }
     
 }
