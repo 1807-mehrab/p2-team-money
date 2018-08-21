9dd0215ec6a0e75cd413e2e9198b4faca9e656b8
christian <cshelby77@gmail.com>
2018-08-20 21:49:23 -0500

    Some DAO fixes, no transaction implementation

--- StockInvestment/src/main/java/com/revature/stockinvestment/dao/CompanyDaoOracleSqlImpl.java
+++ StockInvestment/src/main/java/com/revature/stockinvestment/dao/CompanyDaoOracleSqlImpl.java
@@ -5,14 +5,8 @@
  */
 package com.revature.stockinvestment.dao;
 
-import com.revature.stockinvestment.model.Account;
 import com.revature.stockinvestment.model.Company;
 import com.revature.stockinvestment.util.ConnectionUtil;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,34 +21,6 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public class CompanyDaoOracleSqlImpl implements CompanyDao { 
-
-	/*
-    private static final String SQL_INSERT_COMPANY 
-            = "INSERT INTO COMPANY (COMPANY_NAME) "
-            + "VALUES (?, ?)";
-    
-    private static final String SQL_INSERT_COMPANY_ACCOUNTS 
-            = "INSERT INTO TRANSACTION (SHARES, ACCOUNT_ID, COMPANY_ID) "
-            + "VALUES (?, ?, ?)";
-    
-    private static final String SQL_DELETE_COMPANY 
-            = "DELETE FROM COMPANY "
-            + "WHERE COMPANY_ID = ?"; 
-    
-    private static final String SQL_UPDATE_COMPANY 
-            = "UPDATE COMPANY "
-            + "SET COMPANY_NAME = ? "
-            + "WHERE COMPANY_ID = ?";
-    
-    private static final String SQL_SELECT_COMPANY_BY_COMPANY_ID 
-            = "SELECT COMPANY_ID, COMPANY_NAME "
-            + "FROM COMPANY "
-            + "WHERE COMPANY_ID = ?";
-    
-    private static final String SQL_SELECT_ALL_COMPANIES 
-            = "SELECT COMPANY_ID, COMPANY_NAME "
-            + "FROM COMPANY";
-    */
     
     //***************************************
 	private SessionFactory sessionFactory;
@@ -70,32 +36,6 @@ public class CompanyDaoOracleSqlImpl implements CompanyDao {
         Transaction t = s.beginTransaction();
         s.save(companyStock);
         t.commit();
-        
-    	/*
-    	PreparedStatement ps = null;
-        ResultSet rs = null;
-
-        try (Connection conn = ConnectionUtil.getConnection()) {
-            ps = conn.prepareStatement(SQL_INSERT_COMPANY);
-            ps.setString(1, companyStock.getCompanyName());
-            rs = ps.executeQuery();
-            //insertCompanyStockAccounts(companyStock);
-        } catch (SQLException e) {
-            throw new SIPersistenceException("Could not connect to db.", e);
-        } catch (IOException e) {
-            throw new SIPersistenceException("Could not write to db.", e);
-        } finally {
-            try {
-                if (ps != null) {
-                    ps.close();
-                }
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException e) {
-                throw new SIPersistenceException("Could not close db.", e);
-            }
-        }*/
     }
 
     @Override
@@ -113,31 +53,6 @@ public class CompanyDaoOracleSqlImpl implements CompanyDao {
         Transaction t = s.beginTransaction();
         s.save(companyStock);
         t.commit();
-    	/*
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-
-        try (Connection conn = ConnectionUtil.getConnection()) {
-            ps = conn.prepareStatement(SQL_UPDATE_COMPANY);
-            ps.setString(1, companyStock.getCompanyName());
-            ps.setInt(2, companyStock.getCompanyStockId());
-            rs = ps.executeQuery();
-        } catch (SQLException e) {
-            throw new SIPersistenceException("Could not connect to db.", e);
-        } catch (IOException e) {
-            throw new SIPersistenceException("Could not write to db.", e);
-        } finally {
-            try {
-                if (ps != null) {
-                    ps.close();
-                }
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException e) {
-                throw new SIPersistenceException("Could not close db.", e);
-            }
-        }*/
     }
 
     @Override
@@ -154,79 +69,12 @@ public class CompanyDaoOracleSqlImpl implements CompanyDao {
         }
         
         return c;
-    	
-    	/*
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        Company companyStock = null;
-
-        try (Connection conn = ConnectionUtil.getConnection()) {
-            ps = conn.prepareStatement(SQL_SELECT_COMPANY_BY_COMPANY_ID);
-            ps.setInt(1, companyStockId);
-            rs = ps.executeQuery();
-            while (rs.next()) {
-                companyStock = new Company();
-                companyStock.setCompanyStockId(rs.getInt("COMPANY_STOCK_ID"));
-                companyStock.setCompanyName(rs.getString("COMPANY_NAME"));
-            }
-        } catch (SQLException e) {
-            throw new SIPersistenceException("Could not connect to db.", e);
-        } catch (IOException e) {
-            throw new SIPersistenceException("Could not read from db.", e);
-        } finally {
-            try {
-                if (ps != null) {
-                    ps.close();
-                }
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException e) {
-                throw new SIPersistenceException("Could not close db.", e);
-            }
-        }
-        return companyStock;*/
     }
 
     @Override
     public List<Company> getAllCompanyStocks() throws SIPersistenceException {
         Session s = sessionFactory.getCurrentSession();
         return s.createQuery("from company").list();
-        
-        
-    	/*
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        Company companyStock = null;
-        List<Company> companyStocks = new ArrayList<>();
-
-        try (Connection conn = ConnectionUtil.getConnection()) {
-            ps = conn.prepareStatement(SQL_SELECT_ALL_COMPANIES);
-            rs = ps.executeQuery();
-            while (rs.next()) {
-                companyStock = new Company();
-                companyStock.setCompanyStockId(rs.getInt("COMPANY_STOCK_ID"));
-                companyStock.setCompanyName(rs.getString("COMPANY_NAME"));
-                companyStocks.add(companyStock);
-            }
-        } catch (SQLException e) {
-            throw new SIPersistenceException("Could not connect to db.", e);
-        } catch (IOException e) {
-            throw new SIPersistenceException("Could not read from db.", e);
-        } finally {
-            try {
-                if (ps != null) {
-                    ps.close();
-                }
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException e) {
-                throw new SIPersistenceException("Could not close db.", e);
-            }
-        }
-        return companyStocks;*/
     }
-    
-    
+
 }
--- StockInvestment/src/main/java/com/revature/stockinvestment/dao/MemberDaoOracleSqlImpl.java
+++ StockInvestment/src/main/java/com/revature/stockinvestment/dao/MemberDaoOracleSqlImpl.java
@@ -6,12 +6,7 @@
 package com.revature.stockinvestment.dao;
 
 import com.revature.stockinvestment.model.Member;
-import com.revature.stockinvestment.util.ConnectionUtil;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,29 +21,6 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public class MemberDaoOracleSqlImpl implements MemberDao { 
-/*
-    private static final String SQL_INSERT_MEMBER 
-            = "INSERT INTO MEMBER (FIRST_NAME, LAST_NAME, EMAIL, PASSWORD) "
-            + "VALUES (?, ?, ?, ?)";
-    
-    private static final String SQL_DELETE_MEMBER  
-            = "DELETE FROM MEMBER "
-            + "WHERE MEMBER_ID = ?"; 
-    
-    private static final String SQL_UPDATE_MEMBER  
-            = "UPDATE MEMBER "
-            + "SET FIRST_NAME = ?, LAST_NAME = ?, EMAIL = ? , PASSWORD = ? "
-            + "WHERE MEMBER_ID = ?";
-    
-    private static final String SQL_SELECT_MEMBER_BY_MEMBER_ID 
-            = "SELECT MEMBER_ID, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD "
-            + "FROM MEMBER "
-            + "WHERE MEMBER_ID = ?";
-    
-    private static final String SQL_SELECT_ALL_MEMBERS 
-            = "SELECT MEMBER_ID, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD "
-            + "FROM MEMBER";
-*/
 	
     //***************************************
 	private SessionFactory sessionFactory;
@@ -64,68 +36,15 @@ public class MemberDaoOracleSqlImpl implements MemberDao {
         Transaction tx = s.beginTransaction();
         s.save(member);
         tx.commit();
-
-
-    	
-    	/*PreparedStatement ps = null;
-        ResultSet rs = null;
-
-        try (Connection conn = ConnectionUtil.getConnection()) {
-            ps = conn.prepareStatement(SQL_INSERT_MEMBER);
-            ps.setString(1, member.getFirstName());
-            ps.setString(2, member.getLastName());
-            ps.setString(3, member.getEmail());
-            ps.setString(4, member.getPassword());
-            rs = ps.executeQuery();
-        } catch (SQLException e) {
-            throw new SIPersistenceException("Could not connect to db.", e);
-        } catch (IOException e) {
-            throw new SIPersistenceException("Could not write to db.", e);
-        } finally {
-            try {
-                if (ps != null) {
-                    ps.close();
-                }
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException e) {
-                throw new SIPersistenceException("Could not close db.", e);
-            }
-        }*/
     }
 
     @Override
     public void deleteMember(int memberId) throws SIPersistenceException {
+    	Member member = getMemberByMemberId(memberId);
         Session s = sessionFactory.getCurrentSession();
         Transaction t = s.beginTransaction();
         s.delete(member);
-        t.commit();
-    	
-    	/*
-    	PreparedStatement ps = null;
-        ResultSet rs = null;
-
-        try (Connection conn = ConnectionUtil.getConnection()) {
-            ps = conn.prepareStatement(SQL_DELETE_MEMBER);
-            ps.setInt(1, memberId);
-            rs = ps.executeQuery();
-        } catch (SQLException e) {
-            throw new SIPersistenceException("Could not connect to db.", e);
-        } catch (IOException e) {
-            throw new SIPersistenceException("Could not write to db.", e);
-        } finally {
-            try {
-                if (ps != null) {
-                    ps.close();
-                }
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException e) {
-                throw new SIPersistenceException("Could not close db.", e);
-            }
-        }*/
+        t.commit();	
     }
 
     @Override
@@ -134,35 +53,6 @@ public class MemberDaoOracleSqlImpl implements MemberDao {
         Transaction t = s.beginTransaction();
         s.update(member);
         t.commit();
-        
-    	/*
-    	PreparedStatement ps = null;
-        ResultSet rs = null;
-
-        try (Connection conn = ConnectionUtil.getConnection()) {
-            ps = conn.prepareStatement(SQL_UPDATE_MEMBER);
-            ps.setString(1, member.getFirstName());
-            ps.setString(2, member.getLastName());
-            ps.setString(3, member.getEmail());
-            ps.setString(4, member.getPassword());
-            ps.setInt(5, member.getMemberId());
-            rs = ps.executeQuery();
-        } catch (SQLException e) {
-            throw new SIPersistenceException("Could not connect to db.", e);
-        } catch (IOException e) {
-            throw new SIPersistenceException("Could not write to db.", e);
-        } finally {
-            try {
-                if (ps != null) {
-                    ps.close();
-                }
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException e) {
-                throw new SIPersistenceException("Could not close db.", e);
-            }
-        }*/
     }
 
     @Override
@@ -176,84 +66,12 @@ public class MemberDaoOracleSqlImpl implements MemberDao {
         	m = members.get(0);
         }
         return m;
-        
-    	/*
-    	PreparedStatement ps = null;
-        ResultSet rs = null;
-        Member member = null;
-
-        try (Connection conn = ConnectionUtil.getConnection()) {
-            ps = conn.prepareStatement(SQL_SELECT_MEMBER_BY_MEMBER_ID);
-            ps.setInt(1, memberId);
-            rs = ps.executeQuery();
-            while (rs.next()) {
-                member = new Member();
-                member.setMemberId(rs.getInt("MEMBER_ID"));
-                member.setFirstName(rs.getString("FIRST_NAME"));
-                member.setLastName(rs.getString("LAST_NAME"));
-                member.setEmail(rs.getString("EMAIL"));
-                member.setPassword(rs.getString("PASSWORD"));
-            }
-        } catch (SQLException e) {
-            throw new SIPersistenceException("Could not connect to db.", e);
-        } catch (IOException e) {
-            throw new SIPersistenceException("Could not read from db.", e);
-        } finally {
-            try {
-                if (ps != null) {
-                    ps.close();
-                }
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException e) {
-                throw new SIPersistenceException("Could not close db.", e);
-            }
-        }
-        return member;*/
     }
 
     @Override
     public List<Member> getAllMembers() throws SIPersistenceException {
         Session s = sessionFactory.getCurrentSession();
         return s.createQuery("from Member").list();
-             		
-    	/*
-    	PreparedStatement ps = null;
-        ResultSet rs = null;
-        Member member = null;
-        List<Member> members = new ArrayList<>();
-
-        try (Connection conn = ConnectionUtil.getConnection()) {
-            ps = conn.prepareStatement(SQL_SELECT_ALL_MEMBERS);
-            rs = ps.executeQuery();
-            while (rs.next()) {
-                member = new Member();
-                member.setMemberId(rs.getInt("MEMBER_ID"));
-                member.setFirstName(rs.getString("FIRST_NAME"));
-                member.setLastName(rs.getString("LAST_NAME"));
-                member.setEmail(rs.getString("EMAIL"));
-                member.setPassword(rs.getString("PASSWORD"));
-                members.add(member);
-            }
-        } catch (SQLException e) {
-            throw new SIPersistenceException("Could not connect to db.", e);
-        } catch (IOException e) {
-            throw new SIPersistenceException("Could not read from db.", e);
-        } finally {
-            try {
-                if (ps != null) {
-                    ps.close();
-                }
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException e) {
-                throw new SIPersistenceException("Could not close db.", e);
-            }
-        }
-        return members;
-        */
     }
     
 }
