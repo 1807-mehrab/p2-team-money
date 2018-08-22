7249ea275c46ba5beca6bcefbbae5693e2e366e9
Jimmy Duffy <jeduffy3@gmail.com>
2018-08-21 19:56:05 -0500

    Updated Servlet Config

---
+++ StockInvestment/src/main/java/com/revature/stockinvestment/util/MapperConfig.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.revature.stockinvestment.util;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ *
+ * @author James
+ */
+public class MapperConfig extends WebMvcConfigurationSupport {
+    
+    @Override
+    protected void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(mapObject());
+        addDefaultHttpMessageConverters(converters);
+    }
+    
+    @Bean
+    public MappingJackson2HttpMessageConverter mapObject() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.UNWRAP_ROOT_VALUE, true);
+        return new MappingJackson2HttpMessageConverter(mapper);
+    }
+    
+}
