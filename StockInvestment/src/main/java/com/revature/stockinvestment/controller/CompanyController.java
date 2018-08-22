2af2863f8d193cf99062ed0078d1055713a4c8c9
Jimmy Duffy <jeduffy3@gmail.com>
2018-08-22 10:09:50 -0500

    Updated Company Controller

--- StockInvestment/src/main/java/com/revature/stockinvestment/controller/CompanyController.java
+++ StockInvestment/src/main/java/com/revature/stockinvestment/controller/CompanyController.java
@@ -1,8 +1,21 @@
 package com.revature.stockinvestment.controller;
 
+import com.revature.stockinvestment.dao.SIPersistenceException;
+import com.revature.stockinvestment.model.Company;
 import javax.inject.Inject;
-import org.springframework.stereotype.Controller;
-import com.revature.stockinvestment.service.CompanyServiceLayer;
+import com.revature.stockinvestment.service.CompanyServiceLayerImpl;
+import java.util.List;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CompanyController {
@@ -15,37 +28,31 @@ public class CompanyController {
 		 this.cs = cs;
 	 }
 	
-	 /*
-	public void setCs (CompanyServiceLayerImpl cs) {
-		this.cs = cs;
-	}
-	*/
-	 
-	 @RequestMapping("addCompany")
-	 public void addCompanyStock(@RequestBody Company companyStock) throws SIPersistenceException {
-		 cs.addCompanyStock(companyStock);
+	 @PostMapping("/company")
+         @ResponseStatus(HttpStatus.CREATED)
+	 public void addCompany(@Valid @RequestBody Company company) throws SIPersistenceException {
+		 cs.addCompanyStock(company);
 	 }
 	    
-	 @DeleteMapping("remove/{id}")
-	 public void deleteCompanyStock(@PathVariable("id") int companyStockId) throws SIPersistenceException {
-		 cs.deleteCompanyStock(companyStockId);
+	 @DeleteMapping("/company/{id}")
+         @ResponseStatus(HttpStatus.NO_CONTENT)
+	 public void deleteCompany(@PathVariable("id") int id) throws SIPersistenceException {
+		 cs.deleteCompanyStock(id);
 	 }
 	    
-	 @RequestMapping("updateCompany")
-	 public void updateCompanyStock(@RequestBody Company companyStock) throws SIPersistenceException {
-		 cs.updateCompanyStock(companyStock);
+	 @PutMapping("/company/{id}")
+	 public void updateCompanyStock(@PathVariable("id") int id, @Valid @RequestBody Company company) throws SIPersistenceException {
+		 cs.updateCompanyStock(company);
 	 }
 	    
 	 @GetMapping("/company/{id}")
-	 public Company getCompanyStockByCompanyStockId(@PathVariable("id") int companyStockId) throws SIPersistenceException {
-		return cs.getCompanyStockByCompanyStockId(companyStockId);
-		 
+	 public Company getCompanyStockByCompanyStockId(@PathVariable("id") int id) throws SIPersistenceException {
+		return cs.getCompanyStockByCompanyStockId(id);
 	 }
 	    
 	 @GetMapping("/companies")
 	 public List<Company> getAllCompanyStocks() throws SIPersistenceException {
 		return cs.getAllCompanyStocks();
-		 
 	 }
 
 }
