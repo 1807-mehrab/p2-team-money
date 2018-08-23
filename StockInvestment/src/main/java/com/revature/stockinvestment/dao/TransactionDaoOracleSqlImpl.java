9b547b4654ba89cc2796e5b070490d9c50e879de
Jimmy Duffy <jeduffy3@gmail.com>
2018-08-23 14:51:35 -0500

    Updated Transaction mapping

--- StockInvestment/src/main/java/com/revature/stockinvestment/dao/TransactionDaoOracleSqlImpl.java
+++ StockInvestment/src/main/java/com/revature/stockinvestment/dao/TransactionDaoOracleSqlImpl.java
@@ -13,23 +13,22 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
  * @author James
  */
+@Transactional
 @Repository
 public class TransactionDaoOracleSqlImpl {
-    
-	//***************************************
-	private SessionFactory sessionFactory;
 
-	public void setSessionFactory(SessionFactory sessionFactory) {
-		this.sessionFactory = sessionFactory;
-	}
-	//***************************************
-	
-   
+    private SessionFactory sessionFactory;
+
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
     public void addTransaction(Transaction transaction) throws SIPersistenceException {
         Session s = sessionFactory.getCurrentSession();
         org.hibernate.Transaction t = s.beginTransaction();
@@ -37,40 +36,36 @@ public class TransactionDaoOracleSqlImpl {
         t.commit();
     }
 
-
     public void deleteTransaction(int transactionId) throws SIPersistenceException {
-    	Transaction transaction = getTransactionByTransactionId(transactionId);
-    	Session s = sessionFactory.getCurrentSession();
-    	org.hibernate.Transaction t = s.beginTransaction();
-    	s.delete(transaction);
-    	t.commit();
+        Transaction transaction = getTransactionByTransactionId(transactionId);
+        Session s = sessionFactory.getCurrentSession();
+        org.hibernate.Transaction t = s.beginTransaction();
+        s.delete(transaction);
+        t.commit();
     }
 
-    
     public void updateTransaction(Transaction transaction) throws SIPersistenceException {
-    	Session s = sessionFactory.getCurrentSession();
-    	org.hibernate.Transaction t = s.beginTransaction();
-    	s.save(transaction);
-    	t.commit();
+        Session s = sessionFactory.getCurrentSession();
+        org.hibernate.Transaction t = s.beginTransaction();
+        s.save(transaction);
+        t.commit();
     }
 
-    
     public Transaction getTransactionByTransactionId(int transactionId) throws SIPersistenceException {
-    	Transaction transaction = null;
-    	List<Transaction> transactions = new ArrayList<Transaction>();
-    	Session s = sessionFactory.getCurrentSession();
-    	transactions = s.createQuery("from Transaction where transaction_id = :tId")
-    			.setInteger("tId", transactionId).list();
-    	if(!transactions.isEmpty()) {
-    		transaction = transactions.get(0);
-    	}
-    	return transaction;
+        Transaction transaction = null;
+        List<Transaction> transactions = new ArrayList<Transaction>();
+        Session s = sessionFactory.getCurrentSession();
+        transactions = s.createQuery("from Transaction where transaction_id = :tId")
+                .setInteger("tId", transactionId).list();
+        if (!transactions.isEmpty()) {
+            transaction = transactions.get(0);
+        }
+        return transaction;
     }
 
-    
     public List<Transaction> getAllTransactions() throws SIPersistenceException {
-    	Session s = sessionFactory.getCurrentSession();
-    	return s.createQuery("from Transaction").list();
+        Session s = sessionFactory.getCurrentSession();
+        return s.createQuery("from Transaction").list();
     }
-    
+
 }
